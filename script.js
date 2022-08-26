cb1e32da71adafa702980b1e2739591fd61de526
Livia <liviaflore@uol.com.br>
2022-08-26 00:32:11 -0300

    modificações na DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+let Nome = window.document.getElementById('Nome')
+let Email = document.querySelector('#Email')
+let Data = document.querySelector('#Data')
+let Mensagem = document.querySelector('#Mensagem')
+
+Nome.style.width = '100%'
+Email.style.width = '100%'
